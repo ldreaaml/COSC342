00f05292092b4c4e29010753e64dce251779bbc6
ldreaaml <dreamza43@gmail.com>
2021-04-21 12:07:47 +1200

    assignment2 skeleton code

---
+++ Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/AmbientLightSource.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/AmbientLightSource.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Camera.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Camera.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Colour.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Colour.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Cube.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Cube.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Cylinder.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Cylinder.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Direction.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Direction.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/DirectionalLightSource.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/DirectionalLightSource.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/ImageDisplay.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/ImageDisplay.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/LightSource.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/LightSource.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Material.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Material.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Matrix.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Matrix.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Normal.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Normal.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Object.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Object.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/PinholeCamera.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/PinholeCamera.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Plane.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Plane.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Point.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Point.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/PointLightSource.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/PointLightSource.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Scene.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Scene.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/SceneReader.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/SceneReader.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Sphere.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Sphere.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Transform.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Transform.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Tube.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Tube.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/Vector.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/Vector.cpp.o"
+  "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/rayTracerMain.cpp" "/home/cshome/p/pjitsawatpaiboon/Desktop/342/Assignment2/RayTracer2021/build/CMakeFiles/rayTracer.dir/rayTracerMain.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
