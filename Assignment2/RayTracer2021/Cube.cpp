c5f62fd17e57cd3bd2aad066b4038537a8a3a42e
ldreaaml <dreamza43@gmail.com>
2021-04-26 20:33:18 +1200

    Fixed jagged shadow cylinder

--- Assignment2/RayTracer2021/Cube.cpp
+++ Assignment2/RayTracer2021/Cube.cpp
@@ -33,195 +33,185 @@ std::vector<RayIntersection> Cube::intersect(const Ray &ray) const
 	* Code to implement Ray-Cube interesection goes here *
 	*****************************************************/
 
+	//Cube object can be made from 6 planes that are align on different axes
+
 	//apply inverse transormation to ray
 	Ray inverseRay = transform.applyInverse(ray);
 
-	if (true)
-	{
+	/*Two faces parallel to X-Y plane, shift +-1 unit along the Z-axis
+		 z = +-1 = z0 + t(dz), rearrange equation to  t = (+-1)-z0/dz */
+	double z0 = inverseRay.point(2);
+	double dz = inverseRay.direction(2);
+	double t;
 
-		//The cube will be made from 6 planes which align on different axes
-		//[x,y,z] = [x0,y0,z0] + t[dx,dy,dz]
+	//This plane shift 1 unit on Z-axis  z0 + t(dz) = 1
+	t = (1 - z0) / dz;
+	if (std::abs(dz) > epsilon && t > 0){ //check if ray hits the plane
 
-		/*Two faces parallel to X-Y plane, shift +-1 unit along the Z-axis
-		 z = +-1 = z0 + t(dz)  , rearrange equation to  t = (+-1)-z0/dz */
-		double z0 = inverseRay.point(2);
-		double dz = inverseRay.direction(2);
-		double t;
+		RayIntersection hit;
+		hit.point = inverseRay.point + t * inverseRay.direction;
 
-		//This plane shift 1 unit on Z-axis  t = 1-z0/dz
-		t = (1 - z0) / dz;
-		if (std::abs(dz) > epsilon&& t>0)
-		{ //check if ray hits the plane
-			RayIntersection hit;
-			hit.point = inverseRay.point + t * inverseRay.direction; //find the point where ray hit??
+		//check if hit point's X and Y value is in the range [-1,1]
+		if (hit.point(0) >= -1 && hit.point(0) <= 1 && hit.point(1) >= -1 && hit.point(1) <= 1)
+		{
+			hit.material = material;  //hit point material to be the same as Plane's material
+			hit.normal = Normal(0, 0, 1); //XY Plane is perpendicular to z-axis, so the normal is [0 0 1]
 
-			//check if hit point's X and Y value lie in the range [-1,1]
-			if (hit.point(0) >= -1 && hit.point(0) <= 1 && hit.point(1) >= -1 && hit.point(1) <= 1)
+			//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
+			hit.point = transform.apply(hit.point);
+			hit.normal = transform.apply(hit.normal);
+			if (hit.normal.dot(ray.direction) > 0)
 			{
-
-				hit.material = material;	  //hit point material to be the same as Plane's material
-				hit.normal = Normal(0, 0, 1); //Z=0, so hit point normal is [0,0,1] ??
-
-				//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
-				hit.point = transform.apply(hit.point);
-				hit.normal = transform.apply(hit.normal);
-				if (hit.normal.dot(ray.direction) > 0)
-				{
-					hit.normal = -hit.normal;
-				}
-				//calculate distance between ray's starting point and hit point
-				hit.distance = (hit.point - ray.point).norm();
-				result.push_back(hit);
+				hit.normal = -hit.normal;
 			}
+			//calculate distance between ray's starting point and hit point
+			hit.distance = (hit.point - ray.point).norm();
+			result.push_back(hit);
 		}
-		//This plane shift 1 unit on Z-axis  t = 1-z0/dz
-		t = (-1 - z0) / dz;
-		if (std::abs(dz) > epsilon&& t>0)
-		{ //check if ray hits the plane
-			RayIntersection hit;
-			hit.point = inverseRay.point + t * inverseRay.direction; //find the point where ray hit??
+	}
+	//This plane shift -1 unit on Z-axis  z0 + t(dz) = -1
+	t = (-1 - z0) / dz;
+	if (std::abs(dz) > epsilon && t > 0){ //check if ray hits the plane
+		RayIntersection hit;
+		hit.point = inverseRay.point + t * inverseRay.direction;
 
-			//check if hit point's X and Y value lie in the range [-1,1]
-			if (hit.point(0) >= -1 && hit.point(0) <= 1 && hit.point(1) >= -1 && hit.point(1) <= 1)
+		//check if hit point's X and Y value is in the range [-1,1]
+		if (hit.point(0) >= -1 && hit.point(0) <= 1 && hit.point(1) >= -1 && hit.point(1) <= 1)
+		{
+
+			hit.material = material;	  //hit point material to be the same as Plane's material
+			hit.normal = Normal(0, 0, 1); //XY Plane is perpendicular to Z-Axis, so the normal is [0 0 1]
+
+			//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
+			hit.point = transform.apply(hit.point);
+			hit.normal = transform.apply(hit.normal);
+			if (hit.normal.dot(ray.direction) > 0)
 			{
-
-				hit.material = material;	  //hit point material to be the same as Plane's material
-				hit.normal = Normal(0, 0, 1); //Z=0, so hit point normal is [0,0,1]
-
-				//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
-				hit.point = transform.apply(hit.point);
-				hit.normal = transform.apply(hit.normal);
-				if (hit.normal.dot(ray.direction) > 0)
-				{
-					hit.normal = -hit.normal;
-				}
-				//calculate distance between ray's starting point and hit point
-				hit.distance = (hit.point - ray.point).norm();
-				result.push_back(hit);
+				hit.normal = -hit.normal;
 			}
+			//calculate distance between ray's starting point and hit point
+			hit.distance = (hit.point - ray.point).norm();
+			result.push_back(hit);
 		}
+	}
 
-		/*Two faces parallel to X-Z plane, shift +-1 unit along the Y-axis
+
+	/*Two faces parallel to X-Z plane, shift +-1 unit along the Y-axis
 		 y = +-1 = y0 + t(dy)  , rearrange equation to  t = (+-1)-y0/dy */
 
-		double y0 = inverseRay.point(1);
-		double dy = inverseRay.direction(1);
+	double y0 = inverseRay.point(1);
+	double dy = inverseRay.direction(1);
 
-		//This plane shift 1 unit on Z-axis  t = 1-z0/dz
-		t = (1 - y0) / dy;
-		if (std::abs(dy) > epsilon&& t>0)
-		{ //check if ray hits the plane
-			RayIntersection hit;
-			hit.point = inverseRay.point + t * inverseRay.direction; //find the point where ray hit??
+	//This plane shift 1 unit on Y-axis  y0 + t(dy) = 1
+	t = (1 - y0) / dy;
+	if (std::abs(dy) > epsilon && t > 0){ //check if ray hits the plane
+		RayIntersection hit;
+		hit.point = inverseRay.point + t * inverseRay.direction;
 
-			//check if hit point's X and Z value lie in the range [-1,1]
-			if (hit.point(0) >= -1 && hit.point(0) <= 1 && hit.point(2) >= -1 && hit.point(2) <= 1)
+		//check if hit point's X and Z value is in the range [-1,1]
+		if (hit.point(0) >= -1 && hit.point(0) <= 1 && hit.point(2) >= -1 && hit.point(2) <= 1)
+		{
+
+			hit.material = material;	  //hit point material to be the same as Plane's material
+			hit.normal = Normal(0, 1, 0); //XZ Plane is perpendicular to y-axis, so the normal is [0 1 0]
+
+			//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
+			hit.point = transform.apply(hit.point);
+			hit.normal = transform.apply(hit.normal);
+			if (hit.normal.dot(ray.direction) > 0)
 			{
-
-				hit.material = material;	  //hit point material to be the same as Plane's material
-				hit.normal = Normal(0, 1, 0); //normal vector point along Y-axis
-
-				//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
-				hit.point = transform.apply(hit.point);
-				hit.normal = transform.apply(hit.normal);
-				if (hit.normal.dot(ray.direction) > 0)
-				{
-					hit.normal = -hit.normal;
-				}
-
-				//calculate distance between ray's starting point and hit point
-				hit.distance = (hit.point - ray.point).norm();
-				result.push_back(hit);
+				hit.normal = -hit.normal;
 			}
-		}
-		//This plane shift -1 unit on Y-axis  t = 1-z0/dz
-		t = (-1 - y0) / dy;
-		if (std::abs(dy) > epsilon&& t>0)
-		{ //check if ray hits the plane
-			RayIntersection hit;
-			hit.point = inverseRay.point + t * inverseRay.direction; //find the point where ray hit??
 
-			//check if hit point's X and Z value lie in the range [-1,1]
-			if (hit.point(0) >= -1 && hit.point(0) <= 1 && hit.point(2) >= -1 && hit.point(2) <= 1)
+			//calculate distance between ray's starting point and hit point
+			hit.distance = (hit.point - ray.point).norm();
+			result.push_back(hit);
+		}
+	}
+	//This plane shift -1 unit on Y-axis  y0 + t(dy) = -1
+	t = (-1 - y0) / dy;
+	if (std::abs(dy) > epsilon && t > 0)
+	{ //check if ray hits the plane
+		RayIntersection hit;
+		hit.point = inverseRay.point + t * inverseRay.direction; //find the point where ray hit??
+
+		//check if hit point's X and Z value lie in the range [-1,1]
+		if (hit.point(0) >= -1 && hit.point(0) <= 1 && hit.point(2) >= -1 && hit.point(2) <= 1)
+		{
+
+			hit.material = material;	  //hit point material to be the same as Plane's material
+			hit.normal = Normal(0, 1, 0); //XZ Plane is perpendicular to y-axis, so the normal is [0 1 0]
+
+			//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
+			hit.point = transform.apply(hit.point);
+			hit.normal = transform.apply(hit.normal);
+			if (hit.normal.dot(ray.direction) > 0)
 			{
-
-				hit.material = material;	  //hit point material to be the same as Plane's material
-				hit.normal = Normal(0, 1, 0); //normal vector point along Y-axis
-
-				//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
-				hit.point = transform.apply(hit.point);
-				hit.normal = transform.apply(hit.normal);
-				if (hit.normal.dot(ray.direction) > 0)
-				{
-					hit.normal = -hit.normal;
-				}
-				//calculate distance between ray's starting point and hit point
-				hit.distance = (hit.point - ray.point).norm();
-				result.push_back(hit);
+				hit.normal = -hit.normal;
 			}
+			//calculate distance between ray's starting point and hit point
+			hit.distance = (hit.point - ray.point).norm();
+			result.push_back(hit);
 		}
+	}
 
-		/*Two faces parallel to Y-Z plane, shift +-1 unit along the X-axis
+	/*Two faces parallel to Y-Z plane, shift +-1 unit along the X-axis
 		 x = +-1 = x0 + t(dx)  , rearrange equation to  t = (+-1)-x0/dx */
+	double x0 = inverseRay.point(0);
+	double dx = inverseRay.direction(0);
 
-		double x0 = inverseRay.point(0);
-		double dx = inverseRay.direction(0);
+	//This plane shift 1 unit on X-axis  x0 + t(dx) = 1
+	t = (1 - x0) / dx;
+	if (std::abs(dx) > epsilon && t > 0)
+	{ //check if ray hits the plane
+		RayIntersection hit;
+		hit.point = inverseRay.point + t * inverseRay.direction; //find the point where ray hit??
 
-		//This plane shift 1 unit on X-axis  t = 1-x0/dx
-		t = (1 - x0) / dx;
-		if (std::abs(dx) > epsilon&& t>0)
-		{ //check if ray hits the plane
-			RayIntersection hit;
-			hit.point = inverseRay.point + t * inverseRay.direction; //find the point where ray hit??
+		//check if hit point's Y and Z value is in the range [-1,1]
+		if (hit.point(1) >= -1 && hit.point(1) <= 1 && hit.point(2) >= -1 && hit.point(2) <= 1)
+		{
 
-			//check if hit point's Y and Z value lie in the range [-1,1]
-			if (hit.point(1) >= -1 && hit.point(1) <= 1 && hit.point(2) >= -1 && hit.point(2) <= 1)
+			hit.material = material;	  //hit point material to be the same as Plane's material
+			hit.normal = Normal(1, 0, 0); //YZ Plane is perpendicular to x-axis, so the normal is [1 0 0]
+
+			//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
+			hit.point = transform.apply(hit.point);
+			hit.normal = transform.apply(hit.normal);
+			if (hit.normal.dot(ray.direction) > 0)
 			{
-
-				hit.material = material;	  //hit point material to be the same as Plane's material
-				hit.normal = Normal(1, 0, 0); //normal vector point along Y-axis
-
-				//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
-				hit.point = transform.apply(hit.point);
-				hit.normal = transform.apply(hit.normal);
-				if (hit.normal.dot(ray.direction) > 0)
-				{
-					hit.normal = -hit.normal;
-				}
-				//calculate distance between ray's starting point and hit point
-				hit.distance = (hit.point - ray.point).norm();
-				result.push_back(hit);
+				hit.normal = -hit.normal;
 			}
+			//calculate distance between ray's starting point and hit point
+			hit.distance = (hit.point - ray.point).norm();
+			result.push_back(hit);
 		}
+	}
 
-		//This plane shift 1 unit on X-axis  t = 1-x0/dx
-		t = (-1 - x0) / dx;
-		if (std::abs(dx) > epsilon && t>0)
-		{ //check if ray hits the plane
-			RayIntersection hit;
-			hit.point = inverseRay.point + t * inverseRay.direction; //find the point where ray hit??
+	//This plane shift -1 unit on X-axis  x0 + t(dx) = -1
+	t = (-1 - x0) / dx;
+	if (std::abs(dx) > epsilon && t > 0){ //check if ray hits the plane
+		RayIntersection hit;
+		hit.point = inverseRay.point + t * inverseRay.direction;
 
-			//check if hit point's Y and Z value lie in the range [-1,1]
-			if (hit.point(1) >= -1 && hit.point(1) <= 1 && hit.point(2) >= -1 && hit.point(2) <= 1)
+		//check if hit point's Y and Z value is in the range [-1,1]
+		if (hit.point(1) >= -1 && hit.point(1) <= 1 && hit.point(2) >= -1 && hit.point(2) <= 1)
+		{
+
+			hit.material = material;	  //hit point material to be the same as Plane's material
+			hit.normal = Normal(1, 0, 0); //YZ Plane is perpendicular to x-axis, so the normal is [1 0 0]
+
+			//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
+			hit.point = transform.apply(hit.point);
+			hit.normal = transform.apply(hit.normal);
+			if (hit.normal.dot(ray.direction) > 0)
 			{
-
-				hit.material = material;	  //hit point material to be the same as Plane's material
-				hit.normal = Normal(1, 0, 0); //normal vector point along Y-axis
-
-				//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
-				hit.point = transform.apply(hit.point);
-				hit.normal = transform.apply(hit.normal);
-				if (hit.normal.dot(ray.direction) > 0)
-				{
-					hit.normal = -hit.normal;
-				}
-				//calculate distance between ray's starting point and hit point
-				hit.distance = (hit.point - ray.point).norm();
-				result.push_back(hit);
+				hit.normal = -hit.normal;
 			}
+			//calculate distance between ray's starting point and hit point
+			hit.distance = (hit.point - ray.point).norm();
+			result.push_back(hit);
 		}
-
-	} //END IF
+	}
 
 	return result;
 }
--- Assignment2/RayTracer2021/Cylinder.cpp
+++ Assignment2/RayTracer2021/Cylinder.cpp
@@ -36,10 +36,9 @@ std::vector<RayIntersection> Cylinder::intersect(const Ray &ray) const
 
 	const Point &p = inverseRay.point;
 	const Direction &d = inverseRay.direction;
-	// Equation is x^2 + y^2 = 1
-	// x = px + dx(t)  and   y = py + dy(t)  - see full working in the summary report
+	// The equation of curved surface of the cylinder is x^2 + y^2 = 1
+	// please see full working in the summary report
 	double px = p(0), dx = d(0), py = p(1), dy = d(1);
-
 	double a = (dx * dx) + (dy * dy);
 	double b = 2 * ((px * dx) + (py * dy));
 	double c = (px * px) + (py * py) - 1;
@@ -61,16 +60,14 @@ std::vector<RayIntersection> Cylinder::intersect(const Ray &ray) const
 		t = -b / (2 * a);
 		if (t > 0)
 		{
-			hit.point = p + t * d;
+			hit.point = p + t * d; //find hit point
+			Normal norm = p + t * d; //normal is hit point with z=0 ,  [x,y,0]
+			norm(2) = 0;
 			if (hit.point(2) >= -1 && hit.point(2) <= 1)
 			{
 				// Intersection is in front of the ray's start point
-				
 				hit.point = transform.apply(hit.point);
-				Normal norm = p + t * d;
-				norm(2) = 0;
-				
-				hit.normal = transform.apply(norm); //Normal point to ??
+				hit.normal = transform.apply(norm);
 				if (hit.normal.dot(ray.direction) > 0)
 				{
 					hit.normal = -hit.normal;
@@ -82,18 +79,16 @@ std::vector<RayIntersection> Cylinder::intersect(const Ray &ray) const
 		break;
 	case 1:
 		// Two intersections
-
 		t = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
 		if (t > 0)
 		{
-			hit.point = p + t * d;
+			hit.point = p + t * d; 
+			Normal norm = p + t * d;
+			norm(2) = 0;
 			if (hit.point(2) >= -1 && hit.point(2) <= 1)
 			{
 				// Intersection is in front of the ray's start point
 				hit.point = transform.apply(hit.point);
-				Normal norm = p + t * d;
-				norm(2) = 0;
-				
 				hit.normal = transform.apply(norm);
 				if (hit.normal.dot(ray.direction) > 0)
 				{
@@ -108,12 +103,12 @@ std::vector<RayIntersection> Cylinder::intersect(const Ray &ray) const
 		if (t > 0)
 		{
 			hit.point = p + t * d;
+			Normal norm = p + t * d;
+			norm(2) = 0;
 			if (hit.point(2) >= -1 && hit.point(2) <= 1)
 			{
 				// Intersection is in front of th ray's start point
 				hit.point = transform.apply(hit.point);
-				Normal norm = p + t * d;
-				norm(2) = 0;
 				hit.normal = transform.apply(norm);
 				if (hit.normal.dot(ray.direction) > 0)
 				{
@@ -133,67 +128,67 @@ std::vector<RayIntersection> Cylinder::intersect(const Ray &ray) const
 	}
 
 
+	/* cap is a plane but we calculate the boundary to make sure it only render the part that make the cap of cylinder
+	 the equation of the boundary check is x^2 + y^2 <=r^2  where r is the raidus of the cylinder */
 
-	
-	if(true){ 
-		// cap is essentially a plane but we calculate the boundary to make sure it doesn't render the part outside the circle
-		//top cap  = plane with z = 1
-		double z0 = inverseRay.point(2);
-		double dz = inverseRay.direction(2);
-		t = (1 - z0) / dz;
-		if (std::abs(dz) > epsilon && t>0)  // ++ t>0?? 
-		{ 
-			RayIntersection hit;
-			hit.point = inverseRay.point + t * inverseRay.direction; 
-			// boundary x^2 + y^2 < 1   &&  x^2 + y^2 > -1
-			double x = hit.point(0);
-			double y = hit.point(1);
-			if ( (x*x + y*y) >= -1 && (x*x + y*y) <=1)
+	//top cap of cylinder is a XY plane with z = 1
+	//plane equation is z0 + t(dz) = 1
+	double z0 = inverseRay.point(2);
+	double dz = inverseRay.direction(2);
+	t = (1 - z0) / dz;
+	if (std::abs(dz) > epsilon && t > 0){
+
+		RayIntersection hit;
+		hit.point = inverseRay.point + t * inverseRay.direction;
+		double x = hit.point(0);
+		double y = hit.point(1);
+
+		if ((x * x + y * y) <= 1){ //check boundary
+
+			hit.material = material;	  //hit point material to be the same as Plane's material
+			hit.normal = Normal(0, 0, 1); //XY Plane is perpendicular to z-axis, so the normal is [0 0 1]
+
+			//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
+			hit.point = transform.apply(hit.point);
+			hit.normal = transform.apply(hit.normal);
+			if (hit.normal.dot(ray.direction) > 0)
 			{
-				hit.material = material;	  //hit point material to be the same as Plane's material
-				hit.normal = Normal(0, 0, 1); //Z=0, so hit point normal is [0,0,1] ??
-
-				//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
-				hit.point = transform.apply(hit.point);
-				hit.normal = transform.apply(hit.normal);
-				if (hit.normal.dot(ray.direction) > 0)
-				{
-					hit.normal = -hit.normal;
-				}
-				//calculate distance between ray's starting point and hit point
-				hit.distance = (hit.point - ray.point).norm();
-				result.push_back(hit);
+				hit.normal = -hit.normal;
 			}
-		}
-		//bottom cap = plane with z = -1
-		t = (-1 - z0) / dz;
-		if (std::abs(dz) > epsilon && t>0)  // ++ t>0?? 
-		{ 
-			RayIntersection hit;
-			hit.point = inverseRay.point + t * inverseRay.direction; 
-			// x^2 + y^2 < 1   &&  x^2 + y^2 > -1
-			double x = hit.point(0);
-			double y = hit.point(1);
-			if ( (x*x + y*y) >= -1 && (x*x + y*y) <=1)
-			{
-				hit.material = material;	  //hit point material to be the same as Plane's material
-				hit.normal = Normal(0, 0, 1); //Z=0, so hit point normal is [0,0,1] ??
-
-				//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
-				hit.point = transform.apply(hit.point);
-				hit.normal = transform.apply(hit.normal);
-				if (hit.normal.dot(ray.direction) > 0)
-				{
-					hit.normal = -hit.normal;
-				}
-
-				//calculate distance between ray's starting point and hit point
-				hit.distance = (hit.point - ray.point).norm();
-				result.push_back(hit);
-			}
+			//calculate distance between ray's starting point and hit point
+			hit.distance = (hit.point - ray.point).norm();
+			result.push_back(hit);
 		}
 	}
 
+	//top cap of cylinder is a XY plane with z = -1
+	//plane equation is z0 + t(dz) = -1
+	t = (-1 - z0) / dz;
+	if (std::abs(dz) > epsilon && t > 0){
+
+		RayIntersection hit;
+		hit.point = inverseRay.point + t * inverseRay.direction;
+		double x = hit.point(0);
+		double y = hit.point(1);
+		
+		if ((x * x + y * y) <= 1){ //check boundary  
+
+			hit.material = material;	  //hit point material to be the same as Plane's material
+			hit.normal = Normal(0, 0, 1); //XY Plane is perpendicular to z-axis, so the normal is [0 0 1]
+
+			//apply forward transform to it's point and normal to undo inverse transform we applied to the ray
+			hit.point = transform.apply(hit.point);
+			hit.normal = transform.apply(hit.normal);
+			if (hit.normal.dot(ray.direction) > 0)
+			{
+				hit.normal = -hit.normal;
+			}
+
+			//calculate distance between ray's starting point and hit point
+			hit.distance = (hit.point - ray.point).norm();
+			result.push_back(hit);
+		}
+	}
 
 	return result;
 }
